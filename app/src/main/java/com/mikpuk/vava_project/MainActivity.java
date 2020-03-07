21288328ab3584ca76cb138d647fdf6f45fa7abc
Dominik Mikuška <dominik.mikuska@gmail.com>
2020-03-07 23:11:01 +0100

    Combined login and registration logic with UI elements

--- app/src/main/java/com/mikpuk/vava_project/MainActivity.java
+++ app/src/main/java/com/mikpuk/vava_project/MainActivity.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton = null;
+    Button registerButton = null;
+    EditText loginText = null;
+    EditText passwordText = null;
 
     SQLConnector connector = new SQLConnector();
 
@@ -15,8 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Nacitanie UI premennych
+        loginButton = findViewById(R.id.LoginButton);
+        registerButton = findViewById(R.id.button);
+        loginText = findViewById(R.id.editText);
+        passwordText = findViewById(R.id.PasswText);
+
+        //Nastavenie onClick spravani tlacidiel
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logInUser();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regInit();
+            }
+        });
+
         //Na testovanie zatial
-        new Thread() {
+        /*new Thread() {
             public void run() {
                 connector.connectToDB();
                 if(connector.isConnectedToDB()) {
@@ -43,11 +72,50 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println("Not connected to database");
                 }
             }
+        }.start();*/
+    }
+
+    //Pokus o najdenie pouzivatela v DB a nasledne prihlasenie
+    private void logInUser() {
+        final String username = loginText.getText().toString();
+        final String password = passwordText.getText().toString();
+
+        new Thread() {
+            public void run() {
+                if (!connector.isConnectedToDB()) {
+                    connector.connectToDB();
+                }
+                int id = connector.getUserInDB(username, password);
+                //Kontrola riesena zatial takto
+                if (id > 0) {
+                    showToast("Logged in");
+                }
+                else {
+                    showToast("Logging in failed");
+                    //Nasledne spravit prihlasenie s poslanim IDcka
+                }
+            }
         }.start();
     }
-    public void regInit(View view)
+
+    public void regInit()
     {
+        //Zrusenie SQL pripojenia pred prepnutim na druhu scenu
+        connector.closeConnection();
+
         Intent intent = new Intent(this, ReggActivity.class);
         startActivity(intent);
     }
+
+    private void showToast(final String text)
+    {
+        //Toto vyhodi bublinu s infom
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/mikpuk/vava_project/ReggActivity.java
+++ app/src/main/java/com/mikpuk/vava_project/ReggActivity.java
@@ -2,13 +2,89 @@ package com.mikpuk.vava_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ReggActivity extends AppCompatActivity {
+
+    Button registerButton = null;
+    EditText usernameText = null;
+    EditText passwordText1 = null;
+    EditText passwordText2 = null;
+    EditText emailText = null; //Na tomto sme sa dohodli? Ale tak da sa implementovat
+
+    SQLConnector connector = new SQLConnector();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regg2);
+
+        //Nacitanie UI premennych
+        registerButton = findViewById(R.id.button);
+        usernameText = findViewById(R.id.editText2);
+        passwordText1 = findViewById(R.id.editText3);
+        passwordText2 = findViewById(R.id.editText4);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    //Skontrolovanie vstupov (PRIDAT!) a registrovanie do DB
+    private void registerUser()
+    {
+        final String username = usernameText.getText().toString();
+        final String password1 = passwordText1.getText().toString();
+        final String password2 = passwordText2.getText().toString();
+        new Thread() {
+            public void run() {
+                if(!password1.equals(password2)){
+                    showToast("Passwords do not match");
+                    return;
+                }
+                if(!connector.isConnectedToDB())
+                {
+                    connector.connectToDB();
+                }
+                //Kontrola riesena zatial takto
+                if(connector.addUserToDB(username,password1)) {
+                    showToast("User registered");
+                    loadMainMenu();
+                }
+                else {
+                    showToast("Error while registering?");
+                }
+            }
+        }.start();
+    }
+
+    //Zavretie registracneho okna a navrat do hlavneho menu
+    private void loadMainMenu()
+    {
+        //Zavretie SQL pripojenia
+        connector.closeConnection();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //Aby sa pouzivatel nevratil back tlacidlom do registracie
+        startActivity(intent);
+    }
+
+    //Toto vyhodi bublinu s infom
+    private void showToast(final String text)
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
