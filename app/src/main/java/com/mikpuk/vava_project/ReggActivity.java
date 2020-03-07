448cce03ef394d19846d32f04027033677416eb7
David <david.pukanec@inclor.eu>
2020-03-07 11:33:07 +0100

    Registration UI

---
+++ app/src/main/java/com/mikpuk/vava_project/ReggActivity.java
@@ -0,0 +1,14 @@
+package com.mikpuk.vava_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReggActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regg2);
+    }
+}
